bbc6b1d1a3362bd623eb24bf08187c6710d1ae8c
Vaishak <vaishak3366@gmail.com>
2026-07-17 23:48:50 +0530

    started the deref exercise - step 1 char variable created

---
+++ 02-Pointers-and-Memory/deref_exercise.c
@@ -0,0 +1,14 @@
+/* lecture 16 exercise - deref read and write using char pointer */
+
+#include <stdio.h>
+
+int main() {
+
+    /* step 1 - create char variable and initialize to 100 */
+    char var = 100;
+
+    /* step 2 - print address of var */
+    // printf address of var here
+
+    return 0;
+}
