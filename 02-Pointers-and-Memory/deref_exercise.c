0207a6aae396eaf247f9f94201b342b3ae26c669
Vaishak <vaishak3366@gmail.com>
2026-07-17 23:49:08 +0530

    step 2 done , printed address using %p

--- 02-Pointers-and-Memory/deref_exercise.c
+++ 02-Pointers-and-Memory/deref_exercise.c
@@ -8,7 +8,10 @@ int main() {
     char var = 100;
 
     /* step 2 - print address of var */
-    // printf address of var here
+    printf("address of var : %p \n", (void*)&var);
+
+    /* step 3 - create pointer and store address */
+    // char *ptr = &var;
 
     return 0;
 }
